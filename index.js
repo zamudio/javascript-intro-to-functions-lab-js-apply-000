0e6f4b3f77a58a14d5b581372994d8d5c5a32843
gregarious-attribute-6455 <mziacm@gmail.com>
2019-08-26 02:55:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,5 +21,7 @@ function sayHiToGrandma(string) {
   else if (string === string.toUpperCase()) {
     return "YES INDEED!"
   }
-  else if (string === "I love you")
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
 }
